ff4c6d9bd041e499db3f655ce21d869458836e9b
Diego <diegopbcoutinho@gmail.com>
2022-07-16 12:56:54 -0300

    Mais funcionalidades
    Adição de página de login, validação de login, score e tela de restart

--- js/script.js
+++ js/script.js
@@ -1,40 +1,62 @@
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
+const timer = document.querySelector('.timer')
+const spanPlayer = document.querySelector('.player')
 
+
+//Mario pula ao qualquer tecla ser acionada, remove a classe jump depois de 500ms
 const jump = () => {
 	mario.classList.add('jump');
-	
-	setTimeout(()=>{
-		
+
+	setTimeout(() => {
+
 		mario.classList.remove('jump');
-		
+
 	}, 500);
-	
+
 }
 
+//Verificar se o mario bateu no cano
 const loop = setInterval(() => {
-	
+
 	const pipePosition = pipe.offsetLeft;
 	const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
 
-	
+
 	if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
-		
+
 		pipe.style.animation = 'none';
 		pipe.style.left = `${pipePosition}px`;
-		
+
 		mario.style.animation = 'none';
 		mario.style.bottom = `${marioPosition}px`;
-		
+
 		mario.src = ('imagens/game-over.png');
-		mario.style.width='75px';
+		mario.style.width = '75px';
 		mario.style.marginLeft = '50px';
-		
+
 		clearInterval(loop);
+
+		clearInterval(this.loop)
+		alert(`Parabéns, ${spanPlayer.innerHTML}! Seu Score foi: ${timer.innerHTML}`)
 	}
-	
+
 }, 10);
 
+//Temporizador
+const startTimer = () => {
+	this.loop = setInterval(() => {
 
+		const currentTime = +timer.innerHTML
+		timer.innerHTML = currentTime + 1
+
+	}, 1000)
+}
+
+//Nome do jogador, inciar temporizador, carregar cartas
+window.onload = () => {
+	spanPlayer.innerHTML = localStorage.getItem('player')
+	startTimer()
+}
 
 document.addEventListener('keydown', jump);
